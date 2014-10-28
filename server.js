9a93bbe6410e8598d0e01b174b88b7fbe61d5347
Randy Davis <randy_davis@outlook.com>
2014-10-28 11:01:12 -0500

    Add incompleteCount controller function and update app to show the incomplete count

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var app = connect();
+
+var http = require('http');
+
+app.use(serveStatic('../angularjs'));
+app.listen(5000);
+
+
+
 // Wrong server code that does not work!  Fix is in angularjs directory server.js file
 
 // var connect = require('connect');
